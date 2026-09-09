4fd358285c2e86796f23fb6bb7e4699387f6c19b
Pete Goodall <pete@digicratic.com>
2026-09-09 09:51:49 +0100

    Initial commit with hardcoded url for Proton CLI

---
+++ src/lib.rs
@@ -0,0 +1,61 @@
+#[derive(Debug)]
+pub struct Downloads {
+    pub platform: String,
+    pub url: String,
+    pub checksum: String,
+}
+
+fn find_table(http_data: &str) -> Option<(Vec<String>, Vec<Vec<String>>)> {
+    use scraper::{ElementRef, Html, Selector};
+    let css = |selector| Selector::parse(selector).unwrap();
+    let get_cells = |row: ElementRef, selector| {
+        row.select(&css(selector))
+            .map(|cell| cell.inner_html().trim().to_string())
+            .collect()
+    };
+    let html = Html::parse_document(http_data);
+    let table = html.select(&css("table")).next()?;
+    let tr = css("tr");
+    let mut rows = table.select(&tr);
+    let headers = get_cells(rows.next()?, "th");
+    let rows: Vec<_> = rows.map(|row| get_cells(row, "td")).collect();
+    Some((headers, rows))
+}
+
+fn strip_html_tags(s: &str) -> String {
+    let mut data = String::new();
+    let mut inside = false;
+    for c in s.chars() {
+        match c {
+            '<' => inside = true,
+            '>' => inside = false,
+            _ if !inside => data.push(c),
+            _ => {}
+        }
+    }
+    data
+}
+
+pub fn parse_downloads(http_data: &str) -> Option<Vec<Downloads>> {
+    let mut downloads: Vec<Downloads> = Vec::new();
+    if let Some(table) = find_table(http_data) {
+        let (headers, rows) = table;
+        let platform_index = headers.iter().position(|h| h == "Platform")?;
+        let url_index = headers.iter().position(|h| h == "URL")?;
+        let checksum_index = headers.iter().position(|h| h == "Checksum (SHA-512)")?;
+        downloads = rows
+            .into_iter()
+            .map(|row| Downloads {
+                platform: row[platform_index].clone(),
+                url: { let text = row[url_index].clone();
+                       strip_html_tags(&text)
+                     },
+                checksum: { let text = row[checksum_index].clone();
+                            strip_html_tags(&text)
+                        },
+            })
+            .collect();
+    }
+    
+    Some(downloads)
+}
---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use anyhow::Result;
+use reqwest;
+use tokio;
+use proton_upd::parse_downloads;
+use clap::Parser;
+
+/// A simple CLI tool to download the latest version of Proton Drive CLI for a specified platform.
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long,)] // default_value_t = String::from("linux/x64"
+    platform: Option<String>,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let args = Args::parse();
+    let res = reqwest::get("https://proton.me/download/drive/cli/index.html").await?;
+    println!("Status: {}", res.status());
+    //println!("Headers:\n{:#?}", res.headers());
+
+    let body = res.text().await?;
+    let downloads = parse_downloads(&body)
+        .ok_or(anyhow::anyhow!("Failed to parse downloads"))?;
+    // println!("Downloads: {:#?}", downloads);
+    for download in downloads {
+        println!("Platform: {}", download.platform);
+        println!("URL: {}", download.url);
+        println!("Checksum: {}", download.checksum);
+        println!();
+    }
+
+    Ok(())
+}
